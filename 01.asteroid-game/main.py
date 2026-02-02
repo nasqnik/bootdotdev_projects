21519acf120555f5971f6841428719ab1613bdd4
nasqnik <nasqnik@gmail.com>
2026-02-02 11:24:34 +0400

    collisions added

--- 01.asteroid-game/main.py
+++ 01.asteroid-game/main.py
@@ -1,7 +1,12 @@
 import pygame 
-from logger import log_state
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state, log_event
+from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+import sys
+
+
 
 def main():
     ver = pygame.version.ver
@@ -14,7 +19,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable, )
+    asteroid_field = AsteroidField()
 
     # Game loop
     while True:
@@ -26,8 +42,16 @@ def main():
                 return
         
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for a in asteroids:
+            if a.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        for d in drawable:
+            d.draw(screen)
 
         # Update display (should be last)
         pygame.display.flip()
